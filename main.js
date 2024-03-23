5fd2c41989b01738872ba7cbe47a5411e29b57a0
manahilnadeem <163245288+nadeemtahir@users.noreply.github.com>
2024-03-23 08:24:59 -0700

    file of cases

---
+++ main.js
@@ -0,0 +1,15 @@
+//camelCase
+//snake_case
+//PascalCase
+var userName = "manahil"; //e.g. camelCase
+var user_name = "manahil"; //e.g. snake_case
+var UserName = "manahil"; // e.g. PsascalCase
+var isUserExist = true;
+var is_user_exist = false;
+var IsUserExist = 1;
+(function (Array) { return []; });
+var studentsName = ["manahil", "ayesha", "komal", 1, 10, true];
+var firstStudent = studentsName[0];
+console.log(studentsName[4]);
+console.log(studentsName[5]);
+console.log(firstStudent);
---
+++ main.ts
@@ -0,0 +1,19 @@
+//camelCase
+//snake_case
+//PascalCase
+
+let userName = "manahil" //e.g. camelCase
+let user_name = "manahil" //e.g. snake_case
+let UserName = "manahil" // e.g. PsascalCase
+
+let isUserExist =true ;
+let is_user_exist = false;
+let IsUserExist =1;
+
+Array => []
+
+let studentsName =["manahil","ayesha","komal", 1, 10, true];
+let firstStudent= studentsName[0]
+console.log(studentsName[4]);
+console.log(studentsName[5]);
+console.log (firstStudent);
